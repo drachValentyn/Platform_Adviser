90589c22452312b54e5b0b4a75122b79308b035c
Ihor <ihor.trypolskyi@gmail.com>
2019-08-21 16:55:08 +0300

    custom questions flow

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -3,15 +3,15 @@
         <router-link to="/" class="back-link result-link">Back to home</router-link>
         <v-container>
             <div class="quiz">
-<!--Do not show questions under blocks in a flow !!!-->
                 <div v-for="(question, index) in quiz.data" :key="index">
-                    <!-- Hide all questions, show only the one with index === to current question index -->
                         <div v-show="index === questionIndex">
                             <div v-if="question.question_type === 'questionsList'" >
+
                                 <div class="question">
                                     <p class="quiz-index">{{index+1}}/{{quiz.data.length}}</p>
                                         <h3 class="question">{{ question.question }}</h3>
                                 </div>
+
                                 <div class="quiz-list">
                                     <!-- for each response of the current question -->
                                     <div v-for="(response, index2) in question.answers" :key="index2"
@@ -31,53 +31,47 @@
                                     </div>
                                 </div>
                             </div>
-                            <div v-else-if="question.question_type === 'blockOfQuestions'" >
+
+                            <div v-else-if="question.question_type === 'blockOfQuestions'">
                                 <div class="question">
                                     <p class="quiz-index">{{index+1}}/{{quiz.data.length}}</p>
 
-                                    <!--<div v-for="(item, i_index) in quiz.data" :key="i_index">-->
-                                        <!--<div v-if="item.question_id === question.prelead_question[0].p_q_id" >-->
+                                    <div v-for="(obj, o_index) in userResponses" :key="o_index">
 
-                                            <!--<h3 :set="preleadQuestion=item.answers[0].dependent_question[0]" class="question">-->
-                                                <!--{{item.answers[0].dependent_question[0].d_q_title}}-->
-                                            <!--</h3>-->
+                                        <div v-if="obj.question_id === question.prelead_question[0].p_q_id">
 
-                                        <!--</div>-->
-                                    <!--</div>-->
-                                    <div v-for="(item, i_index) in quiz.data" :key="i_index">
-                                        <div v-if="item.question_id === question.prelead_question[0].p_q_id" >
-                                            <div v-for="(obj, o_index) in userResponses" :key="o_index">
-                                                <div v-if="obj.question_id === question.prelead_question[0].p_q_id" >
 
-                                                    <h3 :set="preleadQuestion=obj.dependent_question[0]" class="question">
-                                                        {{preleadQuestion.d_q_title}}
-                                                    </h3>
+                                            <h3 class="question">
+                                                {{obj.dependent_question[0].d_q_title}}
+                                            </h3>
+
+
+                                            <div v-for="(dependent_question, dq_index) in dependent_quiz.data" :key="dq_index"
+                                                 v-if="dependent_question.question_id === obj.dependent_question[0].d_q_id" class="quiz-list">
+
+                                                <div v-for="(response2, index3) in dependent_question.answers" :key="index3"
+                                                     class="quiz-item" :class="{ 'chosen': chosen === index3 }">
+
+                                                    <label class="quiz-label">
+                                                        <input type="radio"
+                                                               v-bind:value="response2"
+                                                               name="index"
+                                                               @change="prev(index3)"
+                                                               v-on:click="onClick(index3, response2, dependent_question.question_id)"
+                                                               v-model="userResponses[index]"
+                                                        >
+
+                                                        {{response2.answer}}
+                                                    </label>
                                                 </div>
                                             </div>
+
 
                                         </div>
                                     </div>
                                 </div>
 
-                                <div v-for="(question2, dq_index) in dependent_quiz.data" :key="dq_index"
-                                     v-if="question2.question_id === preleadQuestion.d_q_id" class="quiz-list">
 
-                                     <div v-for="(response2, index3) in question2.answers" :key="index3"
-                                         class="quiz-item" :class="{ 'chosen': chosen === index3 }">
-
-                                        <label class="quiz-label">
-                                            <input type="radio"
-                                                   v-bind:value="response2"
-                                                   name="index"
-                                                   @change="prev(index3)"
-                                                   v-on:click="onClick(index3, response2, question2.question_id)"
-                                                   v-model="userResponses[index]"
-                                            >
-
-                                            {{response2.answer}}
-                                        </label>
-                                     </div>
-                                </div>
                             </div>
                         </div>
                 </div>
@@ -102,11 +96,9 @@
                 questionIndex: 0,
                 userResponses: Array(),
                 chosen: '',
-                preleadQId: '',
                 currentIndex: '',
                 preleadQuestion: '',
-
-
+                dependentQuestion: '',
             }
         },
 
@@ -135,7 +127,6 @@
 
         methods: {
             prev() {
-                // console.log(this.userResponses);
                 setTimeout(function () {
                     // console.log(this.userResponses[0].id_answer);
                     this.next()
@@ -143,7 +134,6 @@
             },
 
             next() {
-                console.log('next RESPONSE CALLED');
                 console.log(this.userResponses);
 
                 this.questionIndex++;
@@ -154,13 +144,6 @@
                     //this.$router.push('/change-result');
                 }
             },
-
-            // memorizeAnswer(index) {
-            //     console.log('memorize called');
-            //     let dependentAnswer = index;
-            //     return dependentAnswer
-            //
-            // },
 
             onClick(index, response, id) {
                 this.chosen = index;
